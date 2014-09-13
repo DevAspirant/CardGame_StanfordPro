de87a1bbfb27f66dc57b2652e84556acdefd6a82
ammar falmban <devAspirant@gmail.com>
2014-09-13 19:43:38 +0900

    Initial Commit

---
+++ CardGame_StanfordPro/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CardGame_StanfordPro
+//
+//  Created by ammar falmban on 9/13/14.
+//  Copyright (c) 2014 devAspirant.wordpress.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
